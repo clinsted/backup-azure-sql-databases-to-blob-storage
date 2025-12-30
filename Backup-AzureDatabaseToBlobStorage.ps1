fa58b31623eb15afb810f89797e1b2b0328721c1
Clint Stedman <clint@cogni.co.za>
2025-12-30 14:41:29 +0200

    new params for storage and database resource group name
    added Create-Blob-Container-If-Not-Exists function
    added reusable storage context

--- Backup-AzureDatabaseToBlobStorage.ps1
+++ Backup-AzureDatabaseToBlobStorage.ps1
@@ -1,40 +1,40 @@
 ﻿<#
 .SYNOPSIS
-	This Azure Automation runbook automates Azure SQL database backup to Blob storage and deletes old backups from blob storage. 
+	This Azure Automation runbook automates Azure SQL database backup to Blob storage and deletes old backups from blob storage.
 
 .DESCRIPTION
-	You should use this Runbook if you want manage Azure SQL database backups in Blob storage. 
-	This runbook can be used together with Azure SQL Point-In-Time-Restore.
+	You should use this Runbook if you want manage Azure SQL database backups in Blob storage.
 
-	This is a PowerShell runbook, as opposed to a PowerShell Workflow runbook.
+.PARAMETER DatabaseResourceGroupName
+	Specifies the name of the resource group where the Azure SQL Database server is located.
 
-.PARAMETER ResourceGroupName
-	Specifies the name of the resource group where the Azure SQL Database server is located
-	
 .PARAMETER DatabaseServerName
-	Specifies the name of the Azure SQL Database Server which script will backup
+	Specifies the name of the Azure SQL Database Server which script will backup.
 
 .PARAMETER DatabaseName
-	Specifies the name of the database which script will backup
+	Specifies the name of the Azure SQL Database which script will backup.
 
 .PARAMETER DatabaseAdminUsername
-	Specifies the administrator username of the Azure SQL Database Server
+	Specifies the administrator username of the Azure SQL Database Server.
 
 .PARAMETER DatabaseAdminPassword
-	Specifies the administrator password of the Azure SQL Database Server
+	Specifies the administrator password of the Azure SQL Database Server.
+
+.PARAMETER StorageResourceGroupName
+	Specifies the name of the resource group where the Azure Storage Account is located.
 
 .PARAMETER StorageKey
-	Specifies the storage key of the storage account
+	Specifies the storage key of the storage account.
 
 .PARAMETER StorageAccountName
-	Specifies the name of the storage account where backup file will be uploaded
+	Specifies the name of the storage account where backup file will be uploaded.
 
 .PARAMETER BlobContainerName
 	Specifies the container name of the storage account where backup file will be uploaded. Container will be created if it does not exist.
 
 .PARAMETER RetentionDays
-	If provided it specifies the number of days how long backups are kept in blob storage. Script will remove all older backup files for each DatabaseName from the container. No other files that may exist in the folder will get deleted.
-	If not provided or < 0 then no files are removed
+	Specifies the number of days backups are kept in blob storage. All older backup files for the Azure SQL Database DatabaseName are deleted from the container.
+	If not provided or < 0 then no deletes are performed.
 
 .INPUTS
 	None.
@@ -44,24 +44,26 @@
 #>
 
 param(
-    [parameter(Mandatory=$true)]
-	[String] $ResourceGroupName,
-    [parameter(Mandatory=$true)]
+ 	[parameter(Mandatory=$true)]
+	[String] $DatabaseResourceGroupName,
+ 	[parameter(Mandatory=$true)]
 	[String] $DatabaseServerName,
 	[parameter(Mandatory=$true)]
-    [String]$DatabaseName,
-    [parameter(Mandatory=$true)]
-    [String]$DatabaseAdminUsername,
+	[String]$DatabaseName,
 	[parameter(Mandatory=$true)]
-    [String]$DatabaseAdminPassword,
-    [parameter(Mandatory=$true)]
-    [String]$StorageKey,
-    [parameter(Mandatory=$true)]
-    [String]$StorageAccountName,
+	[String]$DatabaseAdminUsername,
 	[parameter(Mandatory=$true)]
-    [string]$BlobContainerName,
+	[String]$DatabaseAdminPassword,
+	[parameter(Mandatory=$true)]
+	[String] $StorageResourceGroupName,
+	[parameter(Mandatory=$true)]
+ 	[String]$StorageKey,
+	[parameter(Mandatory=$true)]
+	[String]$StorageAccountName,
+	[parameter(Mandatory=$true)]
+	[string]$BlobContainerName,
 	[parameter(Mandatory=$false)]
-    [Int32]$RetentionDays = 0
+	[Int32]$RetentionDays = 0
 )
 
 $ErrorActionPreference = 'stop'
@@ -79,16 +81,30 @@ function Login() {
 	}
 }
 
-function Export-To-Blob-Storage([string]$resourceGroupName, [string]$databaseServerName, [string]$databaseName, [string]$databaseAdminUsername, [string]$databaseAdminPassword, [string]$storageKey, [string]$storageAccountName, [string]$blobContainerName) {
+function Create-Blob-Container-If-Not-Exists([string]$blobContainerName, $context) {
+	Write-Verbose "Checking if blob container '$blobContainerName' already exists" -Verbose
+
+	# Attempt to get the container and capture any error silently
+	$container = Get-AzStorageContainer -Name $blobContainerName -Context $context -ErrorAction SilentlyContinue
+
+	# Check if the $container variable is null
+	if ($null -eq $container) {
+		New-AzStorageContainer -Name $blobContainerName -Context $context
+		Write-Verbose "Container '$blobContainerName' created" -Verbose
+	} else {
+		Write-Verbose "Container '$blobContainerName' already exists" -Verbose
+	}
+}
+
+function Export-To-Blob-Storage([string]$resourceGroupName, [string]$databaseServerName, [string]$databaseName, [string]$databaseAdminUsername, [string]$databaseAdminPassword, [string]$storageKey, [string]$storageUri) {
 	Write-Verbose "Starting database export database '$databaseName'" -Verbose
+
 	$securePassword = ConvertTo-SecureString -String $databaseAdminPassword -AsPlainText -Force 
 	$creds = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $databaseAdminUsername, $securePassword
-	$bacpacFilename = $databaseName + (Get-Date).ToString("yyyyMMddHHmm") + ".bacpac"
-	$bacpacUri = "https://" + $storageAccountName + ".blob.core.windows.net/" + $blobContainerName + "/" + $databaseName + "/" + $bacpacFilename
 
-	$exportRequest = New-AzSqlDatabaseExport -ResourceGroupName $resourceGroupName -ServerName $databaseServerName -DatabaseName $databaseName -StorageKeytype "StorageAccessKey" -StorageKey $storageKey -StorageUri $bacpacUri -AdministratorLogin $creds.UserName -AdministratorLoginPassword $creds.Password
-	
+	$exportRequest = New-AzSqlDatabaseExport -ResourceGroupName $resourceGroupName -ServerName $databaseServerName -DatabaseName $databaseName -StorageKeytype "StorageAccessKey" -StorageKey $storageKey -StorageUri $storageUri -AdministratorLogin $creds.UserName -AdministratorLoginPassword $creds.Password	
 	$exportStatus = Get-AzSqlDatabaseImportExportStatus -OperationStatusLink $exportRequest.OperationStatusLink
+
 	[Console]::Write("Exporting")
 	while ($exportStatus.Status -eq "InProgress")
 	{
@@ -100,12 +116,11 @@ function Export-To-Blob-Storage([string]$resourceGroupName, [string]$databaseSer
 	$exportStatus
 }
 
-function Delete-Old-Backups([string]$resourceGroupName, [string]$storageAccountName, [int]$retentionDays, [string]$databaseName, [string]$blobContainerName) {
-	$context = (Get-AzStorageAccount -ResourceGroupName $resourceGroupName -Name $storageAccountName).Context
+function Delete-Old-Backups([int]$retentionDays, [string]$databaseName, [string]$blobContainerName, $context) {
 	$isOldDate = [DateTime]::UtcNow.AddDays(-$retentionDays)
-	$bacpacFilename = $databaseName + "/" + $databaseName
+	$prefix = $databaseName + "/" + $databaseName
 
-	$blobs = Get-AzStorageBlob -Container $blobContainerName -Prefix $bacpacFilename -Context $context | Where-Object { $_.Name -like "*.bacpac" }
+	$blobs = Get-AzStorageBlob -Container $blobContainerName -Prefix $prefix -Context $context | Where-Object { $_.Name -like "*.bacpac" }
 	foreach ($blob in ($blobs | Where-Object { $_.LastModified.UtcDateTime -lt $isOldDate -and $_.BlobType -eq "BlockBlob" })) {
 		Write-Verbose ("Removing blob: " + $blob.Name) -Verbose
 		Remove-AzStorageBlob -Blob $blob.Name -Container $blobContainerName -Context $context
@@ -116,23 +131,29 @@ Write-Verbose "Starting database backup" -Verbose
 
 Login
 
+$context = (Get-AzStorageAccount -ResourceGroupName $StorageResourceGroupName -Name $StorageAccountName).Context
+
+Create-Blob-Container-If-Not-Exists `
+	-blobContainerName $BlobContainerName `
+	-context $context
+
+$storageUri = "https://" + $StorageAccountName + ".blob.core.windows.net/" + $BlobContainerName + "/" + $DatabaseName + "/" + $DatabaseName + (Get-Date).ToString("yyyyMMddHHmm") + ".bacpac"
+
 Export-To-Blob-Storage `
-	-resourceGroupName $ResourceGroupName `
+	-resourceGroupName $DatabaseResourceGroupName `
 	-databaseServerName $DatabaseServerName `
 	-databaseName $DatabaseName `
 	-databaseAdminUsername $DatabaseAdminUsername `
 	-databaseAdminPassword $DatabaseAdminPassword `
 	-storageKey $StorageKey `
-	-storageAccountName $StorageAccountName `
-	-blobContainerName $BlobContainerName
+	-storageUri $storageUri
 
 if ($RetentionDays -gt 0) {
 	Delete-Old-Backups `
-		-resourceGroupName $ResourceGroupName `
-		-storageAccountName $StorageAccountName `
 		-retentionDays $RetentionDays `
 		-databaseName $DatabaseName `
-		-blobContainerName $BlobContainerName
+		-blobContainerName $BlobContainerName `
+		-context $context
 }
 
 Write-Verbose "Database backup script finished" -Verbose
